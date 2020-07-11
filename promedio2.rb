16e28250c49ffc5db4cf51524f2c6d03a38157af
KiraElric <lissette.urso@gmail.com>
2020-07-11 19:54:41 -0400

    Desafio Promedio2

---
+++ promedio2.rb
@@ -0,0 +1,18 @@
+def promedio(array)
+  sum_array = array.inject(0){ |sum, elements| sum += elements}
+  length_array = array.length
+  return sum_array/length_array
+end
+
+def compara_arrays(array_1, array_2)
+  average_array_1 = promedio(array_1)
+  average_array_2 = promedio(array_2)
+  if average_array_1 > average_array_2
+    return average_array_1
+  else
+    return average_array_2
+  end
+end
+
+visitas = [500,1000,1500,2000]
+numeros = [1,2,3,4,5]
